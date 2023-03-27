6b6e33bc4f76011849e0e6d00831aea40edc252f
A-Vause <adam.p.vause@gmail.com>
2023-03-27 10:02:15 +0100

    turtle project complete

---
+++ circles.py
@@ -0,0 +1,10 @@
+from turtle import *
+
+circle(50)
+forward(100)
+circle(50)
+color("red")
+circle(50)
+color("#bb00ff")
+circle(60)
+done()
---
+++ main.py
@@ -0,0 +1,8 @@
+from turtle import *
+
+circle(50)
+penup()
+forward(120)
+pendown()
+circle(50)
+done()
